1babe99094df3dfc79320af13b65c980834db072
Jacob Wright <mee@mailinator.com>
2017-12-16 22:34:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kitten.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittn.shift();
+}
